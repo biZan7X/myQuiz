51cb31178c8ec54281d750d4ff04d7f4f4735115
Biswanath Tewari <40662234+biZan7X@users.noreply.github.com>
2021-01-28 21:57:39 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import react from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const questions = [
+	{
+		question: "who is the founder of tesla?",
+		option1: "Elon Musk",
+		option2: "Tony Stark",
+		option3: "Bill Gates",
+		option4: "Nikola Tesla",
+	},
+	{
+		question: "which is the most valuable IT company in the world ?",
+		option1: "Wipro",
+		option2: "Infosys",
+		option3: "TCS",
+		option4: "CTS",
+	},
+	{
+		question: "who is father of computer science",
+		option1: "charles Babbage",
+		option2: "Bill Gates",
+		option3: "Allen turing",
+		option4: "Dennis Ritchie ",
+	},
+];
+
+const App = () => {
+	return <div className="container">{questions[0].question}</div>;
+};
 
 export default App;
